7e3d4d1a768ffabeb271ae46220e1445abbebd33
Tobiasz Hołtyn <tobiasz.github@gmail.com>
2022-06-29 14:56:45 +0200

    update description for flow update-configs

--- cmd/flow_update-configs.go
+++ cmd/flow_update-configs.go
@@ -17,14 +17,10 @@ import (
 
 // updateConfigsCmd represents the updateConfigs command
 var updateConfigsCmd = &cobra.Command{
-	Use:   "update-configs",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "update-configs [flow id]",
+	Short: "Update configuration parameters of an integration flow",
+	Long: `You can use the following command to update the value
+for a configuration parameters of a designtime integration flow.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		var allConfigParams []model.FlowConfigurationPrinter
